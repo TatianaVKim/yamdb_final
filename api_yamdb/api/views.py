dcf30bd583e6e29f494cd9296f6ffea17303e857
Tatyana <tatyana.arch@mail.ru>
2022-08-22 02:56:07 +0600

    add bot_8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,6 +13,7 @@ from reviews.models import Category, Genre, Review, Title
 from users.models import User
 
 
+# t
 class UserSignupView(CreateAPIView):
     model = User
     permission_classes = [rest_permissions.AllowAny]
